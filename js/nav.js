a84d8acac18068f90b936bd385aab6c30b74254c
abishekre <73466571+abishekre@users.noreply.github.com>
2022-07-18 00:38:05 +0530

    int mod 2

--- js/nav.js
+++ js/nav.js
@@ -8,19 +8,19 @@ let navarea = document.getElementById("navarea");
 
 let home = document.getElementById("home");
 const nav1 = document.getElementById("nav1");
-// const nav2 = document.getElementById("nav2");
+const nav2 = document.getElementById("nav2");
 const nav3 = document.getElementById("nav3");
 const nav4 = document.getElementById("nav4");
 
 nav1.style.display = 'none';
-// nav2.style.display = 'none';
+nav2.style.display = 'none';
 nav3.style.display = 'none';
 nav4.style.display = 'none';
 
 homebtn.addEventListener('click',function(){
     console.log('home-btn');
     nav1.style.display = 'none';
-    // nav2.style.display = 'none';
+    nav2.style.display = 'none';
     nav3.style.display = 'none';
     nav4.style.display = 'none';
     home.style.display = 'inline';
@@ -30,7 +30,7 @@ homebtn.addEventListener('click',function(){
 btn1.addEventListener('click',function(){
     console.log('btn-1-dif');
     nav1.style.display = 'inline';
-    // nav2.style.display = 'none';
+    nav2.style.display = 'none';
     nav3.style.display = 'none';
     nav4.style.display = 'none';
     home.style.display = 'none';
@@ -39,7 +39,7 @@ btn1.addEventListener('click',function(){
 btn2.addEventListener('click',function(){
     console.log('btn-2');
     nav1.style.display = 'none';
-    // nav2.style.display = 'inline';
+    nav2.style.display = 'inline';
     nav3.style.display = 'none';
     nav4.style.display = 'none';
     home.style.display = 'none';
@@ -48,7 +48,7 @@ btn2.addEventListener('click',function(){
 btn3.addEventListener('click',function(){
     console.log('btn-3');
     nav1.style.display = 'none';
-    // nav2.style.display = 'none';
+    nav2.style.display = 'none';
     nav3.style.display = 'block';
     nav4.style.display = 'none';
     home.style.display = 'none';
@@ -57,7 +57,7 @@ btn3.addEventListener('click',function(){
 btn4.addEventListener('click',function(){
     console.log('btn-4');
     nav1.style.display = 'none';
-    // nav2.style.display = 'none';
+    nav2.style.display = 'none';
     nav3.style.display = 'none';
     nav4.style.display = 'inline';
     home.style.display = 'none';
