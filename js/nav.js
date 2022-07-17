752bff0ae8164113dca262787e8310a6972e0737
abishekre <73466571+abishekre@users.noreply.github.com>
2022-07-17 12:48:15 +0530

    integration

---
+++ js/nav.js
@@ -0,0 +1,32 @@
+let homebtn =  document.getElementById("home-btn");
+let btn1 =  document.getElementById("btn-mod-1");
+let btn2 =  document.getElementById("btn-mod-2");
+let btn3 =  document.getElementById("btn-mod-3");
+let btn4 =  document.getElementById("btn-mod-4");
+let navarea = document.getElementById("navarea");
+
+homebtn.addEventListener('click',function(){
+    console.log('home-btn');
+    navarea.innerHTML = '';
+    navarea.innerHTML = '<div><img class="home-img" src="assets/icons/icon128.png" alt="Digital WellBeing Logo">  </div>  <div class="home-text">    <label class="home-head">Digital WellBeing</label>    <label class="home-sub">Make work in Internet work for you</label>  </div>';
+});
+
+btn1.addEventListener('click',function(){
+    console.log('btn-1');
+    navarea.innerHTML='<div id="top">    <form id="newSessionForm">        <div class="newses">            <input id="newSessionName" class="newSessionName" type="text" placeholder="Name of the Session">            <button id="submit-btn" class="submit-btn" value="Save">        </div>    </form></div><div class="mid" id="mid">    </div>';
+});
+
+btn2.addEventListener('click',function(){
+    console.log('btn-2');
+    navarea.innerHTML = '';
+});
+
+btn3.addEventListener('click',function(){
+    console.log('btn-3');
+    navarea.innerHTML = '';
+});
+
+btn4.addEventListener('click',function(){
+    console.log('btn-4');
+    navarea.innerHTML='<div class="option1"><label class="option-text" for="auto_skip_ads">Disable Ads</label><div class="check"><label class="switch"> <input type="checkbox" id="auto_skip_ads" class="auto_skip_ads" value="auto_skip_ads" unchecked />    <span class="slider round"></span></label></div></div>  <div class="option2">    <label class="option-text" for="remove_entire_sidebar">      Center contents    </label>    <div class="check">      <label class="switch">        <input type="checkbox" id="remove_entire_sidebar" class="remove_entire_sidebar" value="remove_entire_sidebar" unchecked />        <span class="slider round"></span>      </label>    </div>  </div>    <div class="option3">    <label class="option-text" for="remove_comments">      Disable Comments    </label>    <div class="check">      <label class="switch">        <input type="checkbox" id="remove_comments" class="remove_comments" value="remove_comments" unchecked />        <span class="slider round"></span>      </label>    </div>  </div>  <div class="manage_history-btn" id="manage_history_container">    <a id="manage_history" href="https://myactivity.google.com/product/youtube" target="_blank"><label class="manage_history-text">Manage YouTube History</label> </a>  </div>'
+});
