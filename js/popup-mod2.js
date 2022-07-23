0928a646c5225b609db86f767d6fee7e9a484f84
abishekre <73466571+abishekre@users.noreply.github.com>
2022-07-24 00:44:16 +0530

    mod 2 css int

--- js/popup-mod2.js
+++ js/popup-mod2.js
@@ -1,119 +1,155 @@
-bg=chrome.extension.getBackgroundPage();
+bg = chrome.extension.getBackgroundPage();
+let running = false;
+let always_on = false;
+const cb = document.getElementById("always_on");
 
-function startstop()
-{
-	//alert("hi");
-	
-	
-	hours = parseInt(document.getElementById("hh").value); if (isNaN(hours)) hours=0;
-	minutes = parseInt(document.getElementById("mm").value); if (isNaN(minutes)) minutes=0;
-	seconds = parseInt(document.getElementById("ss").value); if (isNaN(seconds)) seconds=0;
-	newtimelimit = 60 * 60 * hours + 60 * minutes + seconds;
-	starttime = new Date();
-	starttime = starttime.getTime();
-	starttime = starttime/1000 | 0;
-	chrome.storage.local.set({'starttime': starttime, 'timelimit': newtimelimit});
-	if (bg.started==1)
-	{
-		// turn off
-		
-		bg.clearalarm();
-		$('#start').removeClass('red').addClass('green').text('Start');
-		$('#pause').removeClass('green').addClass('not-active');
-		clearInterval(newInterval);
-		chrome.contentSettings['notifications'].clear({});
-		
-	}
-	else
-	{
-		// turn on
-		if (newtimelimit > 0)
-		{
-		bg.setalarm(newtimelimit*1000);
-		$('#start').text('Pause').removeClass('green').addClass('red');
-		$('#pause').removeClass('not-active').addClass('green');
-		chrome.contentSettings['notifications'].set({
-			'primaryPattern': '<all_urls>',
-			'setting': 'block'
-		  });
-		startcounter();
-		}
-	}
+function startstop() {
+  //alert("hi");
+
+  hours = parseInt(document.getElementById("hh").value);
+  if (isNaN(hours)) hours = 0;
+  minutes = parseInt(document.getElementById("mm").value);
+  if (isNaN(minutes)) minutes = 0;
+  seconds = parseInt(document.getElementById("ss").value);
+  if (isNaN(seconds)) seconds = 0;
+  newtimelimit = 60 * 60 * hours + 60 * minutes + seconds;
+  starttime = new Date();
+  starttime = starttime.getTime();
+  starttime = (starttime / 1000) | 0;
+  chrome.storage.local.set({ starttime: starttime, timelimit: newtimelimit });
+  if (bg.started == 1) {
+    // turn off
+
+    bg.clearalarm();
+    $("#start").removeClass("red").addClass("green").text("Start");
+    $("#pause").removeClass("green").addClass("not-active");
+    clearInterval(newInterval);
+    if (!cb.checked) {
+      chrome.contentSettings["notifications"].clear({});
+    }
+  } else {
+    // turn on
+    if (newtimelimit > 0) {
+      bg.setalarm(newtimelimit * 1000);
+      $("#start").text("Pause").removeClass("green").addClass("red");
+      $("#pause").removeClass("not-active").addClass("green");
+      chrome.contentSettings["notifications"].set({
+        primaryPattern: "<all_urls>",
+        setting: "block",
+      });
+      startcounter();
+    }
+  }
 }
 
-function stop()
-{
-	bg.clearalarm();
-	clearInterval(newInterval);
-	
-	chrome.contentSettings['notifications'].clear({});
+function stop() {
+  bg.clearalarm();
+  clearInterval(newInterval);
 
-	chrome.storage.local.set({'timelimit': 0 });
-	$('#start').removeClass('red').addClass('green').text('Start');
-	$('#pause').removeClass('green').addClass('not-active');
-	document.getElementById('hh').value='00';
-	document.getElementById('mm').value='00';
-	document.getElementById('ss').value='00';
+  chrome.contentSettings["notifications"].clear({});
+
+  chrome.storage.local.set({ timelimit: 0 });
+  $("#start").removeClass("red").addClass("green").text("Start");
+  $("#pause").removeClass("green").addClass("not-active");
+  document.getElementById("hh").value = "00";
+  document.getElementById("mm").value = "00";
+  document.getElementById("ss").value = "00";
 }
 
-function startcounter()
-{
-	chrome.storage.local.get(['timelimit','starttime'], function(result){
-	currenttime = new Date();
-	currenttime = currenttime.getTime();
-	currenttime = currenttime/1000 | 0;
-	timeleft=result.timelimit-(currenttime-result.starttime);
-	document.getElementById('hh').value=('0'+(timeleft/60/60 | 0)).slice(-2);
-	document.getElementById('mm').value=('0'+((timeleft/60)%60 | 0)).slice(-2);
-	document.getElementById('ss').value=('0'+(timeleft%60)).slice(-2);
-	newInterval=setInterval(function() {
-		currenttime = new Date();
-		currenttime = currenttime.getTime();
-		currenttime = currenttime/1000 | 0;
-		timeleft=result.timelimit-(currenttime-result.starttime);
-		document.getElementById('hh').value=('0'+(timeleft/60/60 | 0)).slice(-2);
-		document.getElementById('mm').value=('0'+((timeleft/60)%60 | 0)).slice(-2);
-		document.getElementById('ss').value=('0'+(timeleft%60)).slice(-2);
-		if (timeleft<1)
-		{
-			clearInterval(newInterval);
-			document.getElementById('hh').value='00';
-			document.getElementById('mm').value='00';
-			document.getElementById('ss').value='00';
-			chrome.contentSettings['notifications'].clear({});
-		$('#start').removeClass('red').addClass('green').text('Start');
-		$('#pause').removeClass('green').addClass('not-active');
-		}
-		}, 1000);
+function startcounter() {
+  chrome.storage.local.get(["timelimit", "starttime"], function (result) {
+    currenttime = new Date();
+    currenttime = currenttime.getTime();
+    currenttime = (currenttime / 1000) | 0;
+    timeleft = result.timelimit - (currenttime - result.starttime);
+    document.getElementById("hh").value = (
+      "0" +
+      ((timeleft / 60 / 60) | 0)
+    ).slice(-2);
+    document.getElementById("mm").value = (
+      "0" +
+      ((timeleft / 60) % 60 | 0)
+    ).slice(-2);
+    document.getElementById("ss").value = ("0" + (timeleft % 60)).slice(-2);
+    newInterval = setInterval(function () {
+      currenttime = new Date();
+      currenttime = currenttime.getTime();
+      currenttime = (currenttime / 1000) | 0;
+      timeleft = result.timelimit - (currenttime - result.starttime);
+      document.getElementById("hh").value = (
+        "0" +
+        ((timeleft / 60 / 60) | 0)
+      ).slice(-2);
+      document.getElementById("mm").value = (
+        "0" +
+        ((timeleft / 60) % 60 | 0)
+      ).slice(-2);
+      document.getElementById("ss").value = ("0" + (timeleft % 60)).slice(-2);
+      if (timeleft < 1) {
+        clearInterval(newInterval);
+        document.getElementById("hh").value = "00";
+        document.getElementById("mm").value = "00";
+        document.getElementById("ss").value = "00";
+        chrome.contentSettings["notifications"].clear({});
+        $("#start").removeClass("red").addClass("green").text("Start");
+        $("#pause").removeClass("green").addClass("not-active");
+      }
+    }, 1000);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const cb = document.getElementById("always_on");
+  let c = 0;
+  while (c % 10 == 0) {
+    c = c + 1;
+    console.log(cb.checked);
+    if (cb.checked) {
+      console.log(" block");
+      chrome.contentSettings["notifications"].set({
+        primaryPattern: "<all_urls>",
+        setting: "block",
+      });
+    } else {
+      // always_on = false;
+      chrome.contentSettings["notifications"].clear({});
+    }
+  }
+  // if(always_on)
+  // document.getElementById(always-on).checked = true;
+  // else
+  // document.getElementById(always-on).checked = false;
+
+  document.getElementById("start").addEventListener("click", startstop);
+  document.getElementById("pause").addEventListener("click", stop);
+  if (bg.started == 1) {
+    $("#start").text("Pause").removeClass("green").addClass("red");
+    $("#pause").removeClass("not-active").addClass("green");
+    running = true;
+    chrome.contentSettings["notifications"].set({
+      primaryPattern: "<all_urls>",
+      setting: "block",
     });
-}
+    startcounter();
+  } else {
+    $("#start").removeClass("red").addClass("green").text("Start");
+    $("#pause").removeClass("green").addClass("not-active");
+    // chrome.contentSettings["notifications"].clear({});
+    running = false;
 
-document.addEventListener('DOMContentLoaded', function () {
-	
-	document.getElementById('start').addEventListener('click', startstop);
-	document.getElementById('pause').addEventListener('click', stop);
-	if (bg.started==1)
-	{
-		$('#start').text('Pause').removeClass('green').addClass('red');
-		$('#pause').removeClass('not-active').addClass('green');
-		chrome.contentSettings['notifications'].set({
-			'primaryPattern': '<all_urls>',
-			'setting': 'block'
-		  });
-		startcounter();
-	}
-	else
-	{
-		
-		$('#start').removeClass('red').addClass('green').text('Start');
-		$('#pause').removeClass('green').addClass('not-active');
-		chrome.contentSettings['notifications'].clear({});
-
-		chrome.storage.local.get({'timelimit': 0}, function(result){
-			document.getElementById('hh').value=('0'+(result.timelimit/60/60 | 0)).slice(-2);
-			document.getElementById('mm').value=('0'+((result.timelimit/60)%60 | 0)).slice(-2);
-			document.getElementById('ss').value=('0'+(result.timelimit%60)).slice(-2);
-		});
-	}
-	
+    chrome.storage.local.get({ timelimit: 0 }, function (result) {
+      document.getElementById("hh").value = (
+        "0" +
+        ((result.timelimit / 60 / 60) | 0)
+      ).slice(-2);
+      document.getElementById("mm").value = (
+        "0" +
+        ((result.timelimit / 60) % 60 | 0)
+      ).slice(-2);
+      document.getElementById("ss").value = (
+        "0" +
+        (result.timelimit % 60)
+      ).slice(-2);
+    });
+  }
 });
