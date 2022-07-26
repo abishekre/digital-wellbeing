103cb579beb26d8f8e5a07936346f0b723167664
abishekre <73466571+abishekre@users.noreply.github.com>
2022-07-26 23:38:28 +0530

    blocked css

--- js/options.js
+++ js/options.js
@@ -29,13 +29,11 @@ function save_options() {
     }
   }
 
-  function timetosecs(times)
-  {
+  function timetosecs(times) {
     var a = times.split(":");
     var sum = 0;
-    sum = parseInt(a[0])*3600 + parseInt(a[1])*60 + parseInt(a[2]);
-    if (sum>86400)
-      return 86400;
+    sum = parseInt(a[0]) * 3600 + parseInt(a[1]) * 60 + parseInt(a[2]);
+    if (sum > 86400) return 86400;
     return sum;
   }
 
@@ -46,9 +44,9 @@ function save_options() {
   var restrictiontimes = restrictiontime1.value.split(/\r?\n/);
   var restrictiontime = [];
   var restrictionlist = [];
-  for (var i = 0; i< restrictiontimes.length; i++) {
+  for (var i = 0; i < restrictiontimes.length; i++) {
     var times = restrictiontimes[i];
-    if(times) {
+    if (times) {
       var secs = timetosecs(times);
       var seconds = secs.toString();
       restrictiontime.push(seconds);
@@ -67,13 +65,13 @@ function save_options() {
   var times = JSON.parse(localStorage["restrictiontime"]);
   var restrictiontimel = document.getElementById("restrictiontime");
   var restrictiontime = [];
-  for( var i = 0; i<times.length; i++) {
+  for (var i = 0; i < times.length; i++) {
     var n = times[i];
     var m = secstotime(n);
     restrictiontime.push(m);
   }
   restrictiontimel.value = restrictiontime.join("\n");
-  
+
   // Check limit data
   var limit_data = document.getElementById("chart_limit");
   var limit = parseInt(limit_data.value);
@@ -85,28 +83,24 @@ function save_options() {
   }
 
   const uname = document.getElementById("username");
-  if(uname)
-  {
+  if (uname) {
     localStorage["uname"] = uname.value;
     uname.value = localStorage["uname"];
-  }
-  else{
+  } else {
     uname.value = localStorage["uname"];
   }
 
   const pass = document.getElementById("passwd");
-  if(uname)
-  {
-    localStorage["pwd"] = uname.value;
-    uname.value = localStorage["pwd"];
-  }
-  else{
-    uname.value = localStorage["pwd"];
+  if (pass) {
+    localStorage["pwd"] = pass.value;
+    pass.value = localStorage["pwd"];
+  } else {
+    pass.value = localStorage["pwd"];
   }
 
   // Update status to let user know options were saved.
   var status = document.getElementById("status");
-  status.innerHTML = "Options Saved.";
+  status.innerHTML = "<div class='options-saved' >Options Saved.</div>";
   status.className = "success";
   setTimeout(function () {
     status.innerHTML = "";
@@ -114,26 +108,18 @@ function save_options() {
   }, 750);
 }
 
-
-function secstotime(times)
-{
+function secstotime(times) {
   var a = parseInt(times);
   var h = 0;
   var m = 0;
-  while(a>0)
-  {
-    if(a>=3600)
-    {
+  while (a > 0) {
+    if (a >= 3600) {
       a -= 3600;
       h += 1;
-    }
-    else if(a>=60)
-    {
+    } else if (a >= 60) {
       a -= 60;
       m += 1;
-    }
-    else
-      break;
+    } else break;
   }
   var hr = h.toString();
   var mt = m.toString();
@@ -153,7 +139,7 @@ function restore_options() {
   var times = JSON.parse(localStorage["restrictiontime"]);
   var restrictiontimel = document.getElementById("restrictiontime");
   var restrictiontime = [];
-  for( var i = 0; i<times.length; i++) {
+  for (var i = 0; i < times.length; i++) {
     var n = times[i];
     var m = secstotime(n);
     restrictiontime.push(m);
@@ -162,16 +148,16 @@ function restore_options() {
   var limitEl = document.getElementById("chart_limit");
   limitEl.value = localStorage["chart_limit"];
 
-  var uname1 = window.localStorage.getItem('uname');
+  var uname1 = window.localStorage.getItem("uname");
   var uname = document.getElementById("username");
   uname.value = uname1;
-  var uname1 = window.localStorage.getItem('pwd');
+  var uname1 = window.localStorage.getItem("pwd");
   var uname = document.getElementById("passwd");
   uname.value = uname1;
   // console.log(JSON.parse(localStorage.getItem('username')));
   // const uname1 = document.getElementById("uname");
   // console.log(localStorage["uname"]);
-//   uname1.value = JSON.parse(localStorage["uname"]);
+  //   uname1.value = JSON.parse(localStorage["uname"]);
 }
 
 // Clear all data except for blacklist
@@ -183,7 +169,6 @@ function clearData() {
   bg.setDefaults();
   location.reload();
 }
-
 
 document.addEventListener("DOMContentLoaded", function () {
   // Restore options
